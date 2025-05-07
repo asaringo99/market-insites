8ab32e767f1f4d55630be39baf0e9ba8bc34fd5f
asaringo99 <ka99tns89@gmail.com>
2025-05-07 10:29:42 +0900

    feat: kernel

---
+++ backend/crates/kernel/src/core.rs
@@ -0,0 +1,18 @@
+use async_trait;
+use anyhow;
+
+#[async_trait::async_trait]
+pub trait ReqSpec: Send + Sync {}
+
+#[async_trait::async_trait]
+pub trait ResSpec: Sized + Send + Sync + 'static {}
+
+#[async_trait::async_trait]
+pub trait Pull<Req: ReqSpec, Res: ResSpec> {
+		async fn pull(&self, req: Req) -> anyhow::Result<Res>;
+}
+
+#[async_trait::async_trait]
+pub trait Push<T> {
+	async fn push(&self, items: &[T]) -> anyhow::Result<()>;
+}
